7578ca9ce451aad936518d57056a2a8f4247bc14
Adarsh Pai <adarsh9pai@gmail.com>
2019-01-19 14:08:48 -0600

    LiveCompile POST request file creation done.

--- src/LiveCompile/public/src/main.js
+++ src/LiveCompile/public/src/main.js
@@ -1,12 +1,46 @@
-"use strict";
+'use strict';
 
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 5000;
+var fs = require('fs');
 
-app.listen(PORT, function (req, res) {
+app.listen(PORT, function (request, response) {
     console.log("Listening at Port", PORT);
 });
-app.use(function (req, res, next) {
-    res.status(404).send({ error: "Not found" });
+app.use(express.json());
+
+app.get('/compile', function (request, response) {
+    response.send("Use POST");
+});
+
+app.post('/compile', function (request, response) {
+    var language = request.body.language,
+        code = request.body.code;
+    var timeStamp = language + Math.round(+new Date() / 1000).toString(10) + request.body.username;
+    console.log(timeStamp);
+    var fileWrite = {
+        'WriteFile': function WriteFile(filename, extension, code) {
+            fs.appendFile(filename + extension, code, function (error) {
+                if (error) throw error;
+                console.log('File' + filename + extension + " created.");
+            });
+        },
+        'Python3': function Python3(filename, code) {
+            fileWrite.WriteFile(filename, '.py', code);
+        },
+        'C++11': function C11(filename, code) {
+            fileWrite.WriteFile(filename, '.cpp', code);
+        },
+        'C': function C(filename, code) {
+            fileWrite.WriteFile(filename, '.c', code);
+        }
+    };
+    fileWrite[language](timeStamp, code);
+
+    response.send('y');
+});
+
+app.use(function (request, response, next) {
+    response.status(404).send({ error: "Not found" });
 });
--- src/LiveCompile/src/main.js
+++ src/LiveCompile/src/main.js
@@ -1,11 +1,43 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+const fs = require('fs')
 
-app.listen(PORT, (req,res)=>{
+app.listen(PORT, (request,response)=>{
     console.log("Listening at Port",PORT)
 })
-app.use((req, res, next)=>{
-    res.status(404).send({error:"Not found"})
-  })
+app.use(express.json())
 
+app.get('/compile',(request,response)=>{
+    response.send("Use POST")
+})
+
+app.post('/compile',(request,response)=>{
+    let language = request.body.language, code = request.body.code
+    let timeStamp = language + (Math.round(+new Date()/1000)).toString(10) + request.body.username
+    console.log(timeStamp)
+    let fileWrite = {
+        'WriteFile':(filename,extension,code)=>{
+            fs.appendFile(filename+extension,code,(error)=>{
+                if(error) throw error
+                console.log('File'+filename+extension+" created.")
+            })
+        },
+        'Python3' : (filename,code)=>{
+            fileWrite.WriteFile(filename,'.py',code)
+        },
+        'C++11' : (filename,code)=>{
+            fileWrite.WriteFile(filename,'.cpp',code)
+        },
+        'C' : (filename,code)=>{
+            fileWrite.WriteFile(filename,'.c',code)
+        }
+    }
+    fileWrite[language](timeStamp,code)
+
+   response.send('y')
+})
+
+app.use((request, response, next)=>{
+    response.status(404).send({error:"Not found"})
+  })
