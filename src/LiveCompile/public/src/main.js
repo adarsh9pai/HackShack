0de5b9904c06b6e0eabd49d72a7ad513f26c90c3
Adarsh Pai <adarsh9pai@gmail.com>
2019-01-19 15:59:33 -0600

    LiveCompile microservice done.

--- src/LiveCompile/public/src/main.js
+++ src/LiveCompile/public/src/main.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 5000;
 var fs = require('fs');
+var exec = require('child_process').exec;
 
 app.listen(PORT, function (request, response) {
     console.log("Listening at Port", PORT);
@@ -18,27 +19,83 @@ app.post('/compile', function (request, response) {
     var language = request.body.language,
         code = request.body.code;
     var timeStamp = language + Math.round(+new Date() / 1000).toString(10) + request.body.username;
-    console.log(timeStamp);
-    var fileWrite = {
-        'WriteFile': function WriteFile(filename, extension, code) {
-            fs.appendFile(filename + extension, code, function (error) {
-                if (error) throw error;
-                console.log('File' + filename + extension + " created.");
-            });
-        },
+
+    var writeCompile = {
         'Python3': function Python3(filename, code) {
-            fileWrite.WriteFile(filename, '.py', code);
+            writeCompile.WriteFile(filename, '.py', code, 'Python3');
         },
         'C++11': function C11(filename, code) {
-            fileWrite.WriteFile(filename, '.cpp', code);
+            writeCompile.WriteFile(filename, '.cpp', code, 'C++11');
         },
         'C': function C(filename, code) {
-            fileWrite.WriteFile(filename, '.c', code);
+            writeCompile.WriteFile(filename, '.c', code, 'C');
+        },
+
+        'WriteFile': function WriteFile(timestamp, extension, code, language) {
+            fs.appendFile(timestamp + extension, code, function (error) {
+                if (error) throw error;
+                console.log('File ' + timestamp + extension + " created.");
+                writeCompile.Compile(timestamp + extension, language, timestamp);
+            });
+        },
+
+        'Compile': function Compile(filename, language, timestamp) {
+            if (language === 'Python3') {
+                exec('python3 ' + filename, function (error, stdout, stderr) {
+                    if (error) {
+                        console.log(error);
+                        response.send({ error: 'There seems to be an error on our side. Please try again later.' });
+                    } else {
+                        response.send({ stdout: stdout, stderr: stderr });
+                        exec('rm -f ' + filename).unref();
+                    }
+                });
+            }
+            if (language === 'C++11') {
+                exec('g++ ' + filename + ' ' + '-o ' + timestamp + 'Executable -std=c++11 ', function (error, stdout, stderr) {
+                    if (error) {
+                        console.log(error);
+                        console.log({ error: 'There seems to be an error on our side. Please try again later.' });
+                    } else {
+                        exec('./' + timestamp + 'Executable', function (error, stdout, stderr) {
+                            if (error) {
+                                console.log(error);
+                                console.log({ error: 'There seems to be an error on our side. Please try again later.' });
+                            } else {
+                                response.send({ Output: stdout });
+                                exec('rm -f ' + filename).unref();
+                                exec('rm -f ' + timestamp + "Executable").unref();
+                                console.log('Executable and file deleted.');
+                            }
+                        });
+                    }
+                });
+            }
+
+            if (language === 'C') {
+                exec('gcc ' + filename + ' -o ' + timestamp + "Executable", function (error, stdout, stderr) {
+                    if (error) {
+                        console.log(error);
+                        console.log({ error: 'There seems to be an error on our side. Please try again later.' });
+                    } else {
+                        exec('./' + timestamp + "Executable", function (error, stdout, stderr) {
+                            if (error) {
+                                console.log(error);
+                                console.log({ error: 'There seems to be an error on our side. Please try again later.' });
+                            } else {
+                                response.send({ Output: stdout });
+                                exec('rm -f ' + filename).unref();
+                                exec('rm -f ' + timestamp + "Executable").unref();
+                                console.log('Executable and file deleted.');
+                            }
+                        });
+                    }
+                });
+            }
         }
     };
-    fileWrite[language](timeStamp, code);
 
-    response.send('y');
+    writeCompile[language](timeStamp, code);
 });
 
 app.use(function (request, response, next) {
--- src/LiveCompile/src/main.js
+++ src/LiveCompile/src/main.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 const fs = require('fs')
+const exec = require('child_process').exec
+
 
 app.listen(PORT, (request,response)=>{
     console.log("Listening at Port",PORT)
@@ -15,27 +17,89 @@ app.get('/compile',(request,response)=>{
 app.post('/compile',(request,response)=>{
     let language = request.body.language, code = request.body.code
     let timeStamp = language + (Math.round(+new Date()/1000)).toString(10) + request.body.username
-    console.log(timeStamp)
-    let fileWrite = {
-        'WriteFile':(filename,extension,code)=>{
-            fs.appendFile(filename+extension,code,(error)=>{
-                if(error) throw error
-                console.log('File'+filename+extension+" created.")
-            })
-        },
+
+    let writeCompile = {
         'Python3' : (filename,code)=>{
-            fileWrite.WriteFile(filename,'.py',code)
+            writeCompile.WriteFile(filename,'.py',code,'Python3')
         },
         'C++11' : (filename,code)=>{
-            fileWrite.WriteFile(filename,'.cpp',code)
+            writeCompile.WriteFile(filename,'.cpp',code,'C++11')
         },
         'C' : (filename,code)=>{
-            fileWrite.WriteFile(filename,'.c',code)
-        }
-    }
-    fileWrite[language](timeStamp,code)
+            writeCompile.WriteFile(filename,'.c',code,'C')
+        },
 
-   response.send('y')
+        'WriteFile':(timestamp,extension,code,language)=>{
+            fs.appendFile(timestamp+extension,code,(error)=>{
+                if(error) throw error
+                console.log('File '+timestamp+extension+" created.")
+                writeCompile.Compile(timestamp+extension,language,timestamp)
+            })
+        },
+
+        'Compile':(filename,language,timestamp)=>{
+            if(language === 'Python3'){
+               exec('python3 '+filename,(error,stdout,stderr)=>{
+                if(error){
+                    console.log(error)
+                    response.send({error:'There seems to be an error on our side. Please try again later.'})
+                }
+                else{
+                    response.send({stdout:stdout,stderr:stderr})
+                    exec('rm -f '+filename).unref()
+                }
+               })
+            }
+            if(language === 'C++11'){
+                exec('g++ '+filename+' '+'-o '+timestamp+'Executable -std=c++11 ',(error,stdout,stderr)=>{
+                    if(error){
+                        console.log(error)
+                        console.log({error:'There seems to be an error on our side. Please try again later.'})
+                    }
+                    else{
+                        exec('./'+timestamp+'Executable',(error,stdout,stderr)=>{
+                            if(error){
+                                console.log(error)
+                                console.log({error:'There seems to be an error on our side. Please try again later.'})
+                            }
+                            else{
+                                response.send({Output:stdout})
+                                exec('rm -f '+filename).unref()
+                                exec('rm -f '+timestamp+"Executable").unref()
+                                console.log('Executable and file deleted.')
+                            }
+                           })        
+                    }
+                   })
+            } 
+
+            if(language === 'C'){
+                exec('gcc '+filename+' -o '+timestamp+"Executable",(error,stdout,stderr)=>{
+                    if(error){
+                        console.log(error)
+                        console.log({error:'There seems to be an error on our side. Please try again later.'})
+                    }
+                    else{
+                        exec('./'+timestamp+"Executable",(error,stdout,stderr)=>{
+                            if(error){
+                                console.log(error)
+                                console.log({error:'There seems to be an error on our side. Please try again later.'})
+                            }
+                            else{
+                                response.send({Output:stdout})
+                                exec('rm -f '+filename).unref()
+                                exec('rm -f '+timestamp+"Executable").unref()
+                                console.log('Executable and file deleted.')
+
+                            }
+                           })        
+                    }
+                   })
+            } 
+        },
+    }
+
+    writeCompile[language](timeStamp,code)
 })
 
 app.use((request, response, next)=>{
